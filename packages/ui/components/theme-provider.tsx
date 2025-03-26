e84801eec31be0e6430930c95d5e20f41783a1ba
densenden <git@deniskreuzer.dk>
2025-03-26 14:03:13 +0100

    fix: Prepare UI package for npm publishing

--- packages/ui/components/theme-provider.tsx
+++ packages/ui/components/theme-provider.tsx
@@ -2,8 +2,20 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+type Attribute = "class" | "data-theme"
+
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: Attribute | Attribute[]
+  defaultTheme?: string
+  enableSystem?: boolean
+  storageKey?: string
+}
+
+export function ThemeProvider({
+  children,
+  ...props
+}: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 } 
