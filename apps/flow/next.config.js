377ccb837b45f25e317b365b551def8caa9a6c32
densenden <git@deniskreuzer.dk>
2025-03-26 17:51:01 +0100

    chore: Update Flow app configuration for Vercel deployment

--- apps/flow/next.config.js
+++ apps/flow/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@sen/ui"],
+  transpilePackages: ["@studiosenmaster/sen-ui"],
+  output: 'standalone'
 }
 
 module.exports = nextConfig 
